23de5dc198f7a5afc0fa37fe2be80a86880a6b43
Hiroshi <citilink25818@gmail.com>
2026-05-26 11:00:21 +0300

    Update train.cpp

--- src/train.cpp
+++ src/train.cpp
@@ -4,68 +4,68 @@
 Train::Train() : stepsCount(0), head(nullptr), ptr(nullptr) {}
 
 Train::~Train() {
-    if (head == nullptr) return;
-    Wagon* current = head->nxt;
-    while (current != head) {
-        Wagon* toDelete = current;
-        current = current->nxt;
-        delete toDelete;
-    }
-    delete head;
+  if (head == nullptr) return;
+  Wagon* current = head->nxt;
+  while (current != head) {
+    Wagon* toDelete = current;
+    current = current->nxt;
+    delete toDelete;
+  }
+  delete head;
 }
 
 void Train::addCar(bool light) {
-    Wagon* newWagon = new Wagon{light, nullptr, nullptr};
-    if (head == nullptr) {
-        head = newWagon;
-        head->nxt = head;
-        head->prv = head;
-        ptr = head;
-    } else {
-        Wagon* last = head->prv;
-        last->nxt = newWagon;
-        newWagon->prv = last;
-        newWagon->nxt = head;
-        head->prv = newWagon;
-    }
+  Wagon* newWagon = new Wagon{light, nullptr, nullptr};
+  if (head == nullptr) {
+    head = newWagon;
+    head->nxt = head;
+    head->prv = head;
+    ptr = head;
+  } else {
+    Wagon* last = head->prv;
+    last->nxt = newWagon;
+    newWagon->prv = last;
+    newWagon->nxt = head;
+    head->prv = newWagon;
+  }
 }
 
 int Train::getLength() {
-    if (head == nullptr) return 0;
-    resetOps();
-    ptr = head;
-    if (ptr->lamp == false) {
-        ptr->lamp = true;
+  if (head == nullptr) return 0;
+  resetOps();
+  ptr = head;
+  if (!ptr->lamp) {
+    ptr->lamp = true;
+  }
+  int length = 0;
+  bool finished = false;
+  while (!finished) {
+    while (true) {
+      ptr = ptr->nxt;
+      ++stepsCount;
+      ++length;
+      if (ptr->lamp) {
+        ptr->lamp = false;
+        break;
+      }
     }
-    int length = 0;
-    bool finished = false;
-    while (!finished) {
-        while (true) {
-            ptr = ptr->nxt;
-            ++stepsCount;
-            ++length;
-            if (ptr->lamp == true) {
-                ptr->lamp = false;
-                break;
-            }
-        }
-        for (int step = 0; step < length; ++step) {
-            ptr = ptr->prv;
-            ++stepsCount;
-        }
-        if (ptr->lamp == false) {
-            finished = true;
-        } else {
-            length = 0;
-        }
+    for (int step = 0; step < length; ++step) {
+      ptr = ptr->prv;
+      ++stepsCount;
     }
-    return length;
+    if (!ptr->lamp) {
+      finished = true;
+    } else {
+      length = 0;
+    }
+  }
+  return length;
 }
 
 int Train::getOpCount() {
-    return stepsCount;
+  return stepsCount;
 }
 
 void Train::resetOps() {
-    stepsCount = 0;
+  stepsCount = 0;
 }
